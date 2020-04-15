d50305c9f72d9d30be1a5b675b3ff3d5e3d760d2
Isaac Mckittrick <mckittri.isaa@gmail.com>
2020-04-15 15:01:48 -0700

    addded variables and function

--- script.js
+++ script.js
@@ -5,10 +5,23 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  // password values
+  var charlow = "abcdefghijklmnopqrstuvwxyz"
+  var charUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var numb = "0123456789"
+  var special = "#$%&\'()*+,-./:;<=>?@[\\]^_`{|}"
   passwordText.value = password;
+// create for loop
 
-}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function writePassword(l,values){
+  var passwordText = '';
+  for(var i = 0; i<l; i++){
+      passwordText += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return passwordText;
+}
