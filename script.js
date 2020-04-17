3b57f6384fa8d09e282b59a9838c560b84ed2c5d
Isaac Mckittrick <mckittri.isaa@gmail.com>
2020-04-17 16:22:38 -0700

    fixed an undefined error with the special charters

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ var special = '~!@#$%^&*()_+=';
 
 var generateBtn = document.getElementById("generate");
 
-//creating an object 4
+//creating an object for user selection
 var userChoices = {};
 
-//prompt character length
+//character length
 var charLength = parseInt(prompt("Type in a number between 8 and 128."));
 
 // userChoice charLength; 
@@ -20,13 +20,55 @@ function promptUser() {
   var numChar = confirm("Include numbers?");
   var lowerChar = confirm("Include lowercase characters?");
   var upperChar = confirm("Include uppercase characters?");
-  var special = confirm("Include special characters?");
+  var specialChar = confirm("Include special characters?");
 
+// saving user's answer 
+  if (numChar) {
+    userChoices["Numb"] = Numb;
+  }
+  if (lowerChar) {
+    userChoices["charlow"] = charlow;
+  }  
+  if (upperChar) {
+    userChoices["charUp"] = charUp;
+  }
+  if (specialChar) {
+    userChoices["special"] = special;
+  }
+  
+  console.log(userChoices);
+}
 
+promptUser();
+
+//
+function writePassword() {
+  let finalPassword = '';
+  for (var i = 0; i < charLength; i++) { 
+
+    let passwordEls = Object.keys(userChoices)
+    let randomEl = passwordEls[Math.floor(Math.random() * passwordEls.length)]
+    let char = getRandomCharacter(userChoices[randomEl]);
+
+    finalPassword += char;
+
+    console.log('RandomCharacter', char);
+    console.log(finalPassword, finalPassword.length);
+    console.log('', charLength)
+
+    
+
+  } 
+  return finalPassword
+}
+
+function getRandomCharacter(str) {
+  return str[Math.floor(Math.random() * str.length)]
+}
 
 writePassword();
 
-//document.getElementById("password").value = writePassword();
+
 
 generateBtn.addEventListener("click", function(){
   password.innerHTML = writePassword();
